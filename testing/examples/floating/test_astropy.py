5f7d26f9072fe3aeb2f032f986c36aebc190884a
Ambra Di Piano <ambra.dipiano@inaf.it>
2022-06-23 10:04:34 +0200

    add alternative units test

--- testing/examples/floating/test_astropy.py
+++ testing/examples/floating/test_astropy.py
@@ -5,6 +5,11 @@ def test_time():
     d = 1 * u.km
     assert u.isclose(d / v, 100 * u.s)
 
+def test_time2():
+    # does not check that you get the exact same unit but that they are convertible
+    v = 1 * u.km / u.hour
+    d = 1 * u.km
+    assert u.isclose(d / v, 3600 * u.s)
 
 def test_many():
     v = 10 * u.m / u.s
