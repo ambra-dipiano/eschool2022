7995ec9d1921bf26164dc879b5e63dd44ed3da07
Ambra Di Piano <ambra.dipiano@inaf.it>
2022-06-23 10:09:47 +0200

    comment scope session vs function

--- testing/examples/fixtures/test_fixtures.py
+++ testing/examples/fixtures/test_fixtures.py
@@ -1,11 +1,19 @@
 import pytest
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def some_data():
     return [1, 2, 3]
+
+def test_mutatable(some_data):
+    # will fail because the first test mutated the array
+    # if scope of fixture is "function" then it will not fail
+    some_data[0] == 5
+    assert some_data[0] == 5
 
 def test_using_fixture(some_data):
     assert len(some_data) == 3
 
 def test_also_using_fixture(some_data):
+    # will not fail with fixture "session"
+    # if scope of fixture is "function" then it will fail
     assert some_data[0] == 1
