9c0b8717955d74c22aaebe3ee60e0dad4df20e99
Ambra Di Piano <ambra.dipiano@inaf.it>
2022-06-23 10:37:45 +0200

    add mark xfail

--- testing/examples/floating/test_floating.py
+++ testing/examples/floating/test_floating.py
@@ -4,5 +4,10 @@ def test_addition_naive():
     # this will fail due to floating points
     assert 0.1 + 0.2 == 0.3
 
+@pytest.mark.xfail
+def test_addition_naive():
+    # this will fail expectedly so we mark it
+    assert 0.1 + 0.2 == 0.3
+
 def test_addition_correct():
     assert 0.1 + 0.2 == pytest.approx(0.3)
