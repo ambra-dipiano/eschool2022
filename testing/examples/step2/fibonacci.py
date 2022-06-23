01e4c20ee0bd07b225cd7e912290acdcf860fdb2
Ambra Di Piano <ambra.dipiano@inaf.it>
2022-06-23 09:55:03 +0200

    add warning and match raised error

--- testing/examples/step2/fibonacci.py
+++ testing/examples/step2/fibonacci.py
@@ -1,6 +1,10 @@
+from logging import warning
+
 def fibonacci(n):
     if n < 0:
         raise ValueError(f'n must be >= 0, got {n}')
+    if n > 40:
+        warning(f'This test is terrible you should keep n < 40, got {n}')
     # rest unchanged
     if n == 0:
         return 0
--- testing/examples/step2/test_exception.py
+++ testing/examples/step2/test_exception.py
@@ -3,5 +3,5 @@ import pytest
 def test_invalid_values():
     from fibonacci import fibonacci
 
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='n must be >= 0, got -1'):
         fibonacci(-1)
